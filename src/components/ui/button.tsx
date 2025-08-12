fa5a47be8664b8740910a1b0492debb0ad2f19ae
Ivan Baravoi <46250545+ivbor@users.noreply.github.com>
2025-08-12 18:50:40 +0200

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none h-11",
   {
     variants: {
       variant: {
@@ -20,10 +20,10 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3 [&_svg]:size-12",
-        lg: "h-11 rounded-md px-8 [&_svg]:size-5",
-        icon: "h-10 w-10",
+        default: "px-4 py-2",
+        sm: "rounded-md px-3",
+        lg: "rounded-md px-8 [&_svg]:size-5",
+        icon: "w-10",
       },
     },
     defaultVariants: {
