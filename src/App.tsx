335dfd1ea65c2c620e86e51fdd5dc87bed296e2e
Ivan Baravoi <46250545+ivbor@users.noreply.github.com>
2025-09-09 15:30:22 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+{ /*
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,7 +18,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+*/}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+{/*
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
@@ -25,3 +29,40 @@ const App = () => (
 );
 
 export default App;
+*/}
+
+import React from 'react'
+import { Header } from './components/Header'
+import { Hero } from './components/Hero'
+import { Benefits } from './components/Benefits'
+import { Audience } from './components/Audience'
+import { CalculatorQuiz } from './components/CalculatorQuiz'
+import { Catalog } from './components/Catalog'
+import { DeliveryPayment } from './components/DeliveryPayment'
+import { Steps } from './components/Steps'
+import { ClientsCarousel } from './components/ClientsCarousel'
+import { ReviewsFlip } from './components/ReviewsFlip'
+import { FAQ } from './components/FAQ'
+import { Footer } from './components/Footer'
+ 
+export default function App() {
+  return <div className="p-6">Hello</div>
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <Header />
+      <main className="flex flex-col">
+        <section id="hero"><Hero /></section>
+        <section id="benefits"><Benefits /></section>
+        <section id="audience"><Audience /></section>
+        <section id="calculator" className="scroll-mt-24"><CalculatorQuiz /></section>
+        <section id="catalog"><Catalog /></section>
+        <section id="delivery-payment"><DeliveryPayment /></section>
+        <section id="steps"><Steps /></section>
+        <section id="clients"><ClientsCarousel /></section>
+        <section id="reviews"><ReviewsFlip /></section>
+        <section id="faq"><FAQ /></section>
+      </main>
+      <Footer />
+    </div>
+  )
+}
