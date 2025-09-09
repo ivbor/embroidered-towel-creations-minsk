d669ff657308b164258451bb441867ee8ab9fb34
Ivan Baravoi <46250545+ivbor@users.noreply.github.com>
2025-09-09 14:45:47 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,6 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+if (!document.getElementById('modal-root')) {
+  const m = document.createElement('div'); m.id = 'modal-root'; document.body.appendChild(m);
+}
