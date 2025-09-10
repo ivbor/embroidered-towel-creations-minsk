87c0d67e2a49a30cb57c987d6bdf85d7b988419f
Ivan Baravoi <46250545+ivbor@users.noreply.github.com>
2025-09-10 20:22:18 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,11 +5,11 @@ export const Hero: React.FC = () => {
   return (
     <div className="border-b bg-gray-50">
       <div className="mx-auto max-w-6xl px-4 py-10 grid md:grid-cols-2 gap-8">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 items-center text-center">
           <h1 className="text-3xl md:text-4xl font-extrabold leading-tight">
             Махровые <span className="text-orange-300">полотенца</span><br /> с индивидуальной вышивкой
           </h1>
-          <ul className="space-y-2 text-2xl list-style-type: circle">
+          <ul className="space-y-2 text-2xl list-disc">
             <li>1000+ готовых макетов вышивки</li>
             <li>Подарочная упаковка</li>
             <li>Изготовление от 1 дня</li>
