ede1980273b6c4f7e84dc2d32bcf021966ccd755
Ivan Baravoi <46250545+ivbor@users.noreply.github.com>
2025-09-11 03:53:05 +0200

    Update Catalog.tsx

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -45,8 +45,8 @@ export const Catalog: React.FC = () => {
   //   </div>
   // </section>
   return (
-  {/* Catalog Section */}
       <section id="catalog" className="py-16 bg-stone-50">
+        {/* Catalog Section */}
         <div className="container mx-auto px-4">
           <div className="text-center mb-12">
             <h2 className="text-3xl font-bold text-gray-900 mb-4">Каталог полотенец</h2>
