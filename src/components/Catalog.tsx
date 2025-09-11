9e423d3007797b7bbf49195da0a05f5d44acdae6
Ivan Baravoi <46250545+ivbor@users.noreply.github.com>
2025-09-11 04:08:21 +0200

    Update Catalog.tsx

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
+import TowelCarousel from '@/components/TowelCarousel';
 import { 
   Phone, 
   Mail, 
@@ -72,6 +73,16 @@ export const Catalog: React.FC = () => {
             </Card>
           </div>
         </div>
+        <TowelCarousel images={[
+        "/lovable-uploads/af3ccc65-18af-4591-b511-5345978cb39a.png",
+        "/lovable-uploads/fdcd1846-7c62-4aeb-9a3d-8838fed3a055.png",
+        "/lovable-uploads/acd0d9a5-e1e3-443c-8727-2d1fb23f1053.png",
+        "/lovable-uploads/f6df139d-aa2a-4952-aa61-d164efa82f24.png",
+        "/lovable-uploads/d6e1ae17-16ae-4e5c-98ec-d209b0b25944.png",
+        "/lovable-uploads/b7db4e1a-ce9d-4dbe-916f-a424ec2185d1.png",
+        "/lovable-uploads/aa44f238-6ef8-4d69-b2c4-97f0543fa51a.png",
+        "/lovable-uploads/ad5bf7b1-2a77-4b15-8e1d-7ef5ae1b36f9.png"
+      ]} />
       </section>
   )
 }
